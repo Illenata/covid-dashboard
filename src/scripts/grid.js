0b7f78219b16fd3cca120300e9f571e80e8e08e2
Silenthill <s-a-w-13@mail.ru>
2020-12-18 02:17:08 +0300

    init: created grid

--- src/scripts/grid.js
+++ src/scripts/grid.js
@@ -1,24 +1,47 @@
 export default class Grid {
-  constructor(map) {
-    this.map = map;
+  constructor() {
     this.grid = null;
     this.section = null;
+    this.global = 100000;
+    this.map = 'Map';
+    this.list = 'list';
+    this.table = 'table';
+    this.graph = 'graph';
   }
 
   init() {
     this.grid = document.createElement('div');
-    this.grid.classlist.add('grid');
-    document.body.append(this.grid);
+    this.grid.classList.add('grid');
+    document.body.prepend(this.grid);
     for (let i = 1; i <= 4; i += 1) {
-      if (i === 1) this.createSections('map');
-      if (i === 1) this.createSections('table');
-      if (i === 1) this.createSections('list');
-      if (i === 1) this.createSections('graph');
+      if (i === 1) this.createSections('head');
+      if (i === 2) this.createSections('left', `${this.list}`, `${this.global}`);
+      if (i === 3) this.createSections('center', `${this.map}`);
+      if (i === 4) this.createSections('right', `${this.table}`, `${this.graph}`);
+      this.grid.append(this.section);
     }
   }
 
-  createSections(x) {
+  createSections(section, firstElement, secondElement) {
     this.section = document.createElement('div');
-    this.section.classList.add(`${x}`);
+    const first = document.createElement('div');
+    const second = document.createElement('div');
+    this.section.classList.add(`${section}`);
+    if (secondElement) {
+      first.innerHTML = `${firstElement}`;
+      second.innerHTML = `${secondElement}`;
+      first.classList.add(`${firstElement}`);
+      second.classList.add(`${secondElement}`);
+      this.section.append(first, second);
+    } else if (firstElement) {
+      this.section.append(firstElement);
+    } else {
+      this.section.innerHTML = 'COVID - 19 DASHBOARD';
+    }
   }
 }
+
+// import map from
+// import table from
+// import list from
+// import graph from
