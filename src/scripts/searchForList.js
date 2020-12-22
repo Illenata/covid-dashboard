f8f52613fa229044139fb5f3e147a77f6fb18bdc
Illenata <khoverko@gmail.com>
2020-12-23 00:46:38 +0200

    feat: implement search for list

--- src/scripts/searchForList.js
+++ src/scripts/searchForList.js
@@ -1,14 +1,35 @@
 export default class Search {
   constructor() {
     this.list = document.querySelector('.list');
-    this.content = document.querySelector('.cases-by-country');
   }
 
   init() {
     const input = document.createElement('input');
     input.type = 'text';
-    input.classList.add('input');
+    input.classList.add('input-search');
     input.placeholder = 'Search';
     this.list.prepend(input);
+
+    input.oninput = function () {
+      const content = document.querySelectorAll('.cases-by-country__item');
+      const value = this.value.trim().toLowerCase();
+
+      if (value) {
+        content.forEach((elem) => {
+          if (elem.innerText.toLowerCase().search(value) === -1) {
+            elem.classList.add('hide');
+          }
+        });
+      } else {
+        content.forEach((elem) => {
+          elem.classList.remove('hide');
+        });
+      }
+    };
   }
+
+  // startSearch() {
+  //   const
+  //   document.querySelector('.input-search')
+  // }
 }
